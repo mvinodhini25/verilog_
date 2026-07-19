ce1d52c02f5a5c5d23798266758e5d86a3c861eb
Vinothini <mvinodhini25@gmail.com>
2026-07-19 22:21:36 +0530

    submit

--- mux/mux4to1_2to1.v
+++ mux/mux4to1_2to1.v
@@ -1,4 +1,4 @@
-module mux(input s,a,b,output y);
+module mux(input s,a,b,output reg y);
 integer i;
 
 always@(*) begin 
@@ -9,4 +9,11 @@ else
 y=a;
 end
 
+endmodule
+
+module mux4(input s1,s0,d0,d1,d2,d3,output m);
+wire w1,w2;
+mux m1(.s(s0),.a(d1),.b(d0),.y(w1));
+mux m2(.s(s0),.a(d3),.b(d2),.y(w2));
+mux m3(.s(s1),.a(w1),.b(w2),.y(m));
 endmodule
