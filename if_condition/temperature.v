fe7f72eba901331ef7d0ddd302af27be3a17eabe
Vinothini <mvinodhini25@gmail.com>
2026-07-20 20:21:45 +0530

    submit

---
+++ if_condition/temperature.v
@@ -0,0 +1,14 @@
+module temp(input a,output reg [47:0]temp);
+always@(*) begin 
+if(a>6'd50) begin 
+temp="hot";
+end
+else if(a<=6'd50 && a>=5'd20) begin 
+temp="normal";
+end
+else begin
+temp="cold";
+end
+
+end
+endmodule
---
+++ if_condition/temperature_tb.v
@@ -0,0 +1,6 @@
+module temp_tb;
+wire temp;
+reg a;
+
+temp tb(.a(a),.temp(temp));
+ initial 
